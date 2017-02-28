fe6f1f64b226ddad0db5f4ff2da7997aa06582a6
Shine Sivadasan <shine.sivadasan@nordstrom.com>
2017-02-28 16:21:40 +0530

    modified

---
+++ if-statement/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	if b := 22; b > 20 {
+		fmt.Println(b)
+	}
+	//we can put assignment statement in if loop. The scope of that variable is present only in if loop
+
+}
