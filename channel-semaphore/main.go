6021c0874f6ed4f96920dd6dfa0dd5b2bd3c7ed2
Shine Sivadasan <shine.sivadasan@nordstrom.com>
2017-03-07 17:25:59 +0530

    modified

---
+++ channel-semaphore/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		done <- true
+	}()
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		done <- true
+	}()
+
+	go func() {
+		<-done
+		<-done
+		close(c)
+		close(done)
+	}()
+
+	for val := range c {
+		fmt.Println(val)
+	}
+}
+
+//Many functions writing to same channel and one function receving the value
